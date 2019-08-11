dc05f4b64103840a00d509c7c2d74471d6bb53ca
Houda Boudiaf <houdaboudiaf@gmail.com>
2019-08-11 23:16:19 +0200

    add create_trip

--- UI/scripts/app.js
+++ UI/scripts/app.js
@@ -4,16 +4,34 @@ sb.addEventListener('click', function(event) {
     var pw = document.getElementById('pw');
     var cpw = document.getElementById('cpw');
     //alert (pw.value+cpw.value);
-    if (pw.value.length>=6) {
-        alert ("correct password length" );
-         if (pw.value == cpw.value) {
-             alert ("password are the same");
+    if (pw.value == cpw.value) {
+       // alert ("password are the same");
 
-         }
-       else {
-           alert ("not the same");
-       }
-    }else {
-        alert("password  is too short");
     }
+  else {
+     // alert ("not the same");
+     var error = document.getElementById('error');
+     error.innerText="passwords are not the same";
+     error.hidden= false;
+  }
+
+});
+
+var pw = document.getElementById('pw');
+pw.addEventListener('blur' ,function() {
+    if (pw.value.length>=6) {
+    //    alert ("correct password length" );
+
+    }else {
+        //alert("password  is too short");
+        var error = document.getElementById('error');
+        error.innerText="password is too short";
+        error.hidden= false;
+
+    }
+});
+
+pw.addEventListener('focus' ,function () {
+    var error = document.getElementById('error');
+    error.innerText="";
 });
